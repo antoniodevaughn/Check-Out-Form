a09a05d9c8eaf7a6512cccd1ca9babde8d2e961d
antoniodevaughn <antonio.devaughn@cdpipelinedevshop.com>
2022-08-01 15:06:45 +0000

    first commit

---
+++ checkout-app/src/App.js
@@ -0,0 +1,11 @@
+import Checkout from './Components/Checkout'
+import './App.css';
+
+function App() {
+  return (
+   
+     <Checkout></Checkout>
+  );
+}
+
+export default App;
---
+++ checkout-app/src/Components/Review.js
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Grid from '@mui/material/Grid';
+
+const products = [
+  {
+    name: 'Product 1',
+    desc: '417 Graphic Rambo Tee',
+    price: '$36.00',
+  },
+  {
+    name: 'Product 2',
+    desc: '443 Long Sleeve Wavy Collection',
+    price: '$38.00',
+  },
+  {
+    name: 'Product 3',
+    desc: '105 Compression Short 2.0',
+    price: '$38.00',
+  },
+  {
+    name: 'Product 4',
+    desc: '224 Stripe Quad Joggers',
+    price: '$38.00',
+  },
+  { name: 'Shipping', desc: '', price: 'Free' },
+];
+
+const addresses = ['1 MUI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
+const payments = [
+  { name: 'Card type', detail: 'Visa' },
+  { name: 'Card holder', detail: 'Antonio DeVaughn' },
+  { name: 'Card number', detail: 'xxxx-xxxx-xxxx-1234' },
+  { name: 'Expiry date', detail: '04/2024' },
+];
+
+export default function Review() {
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        Order summary
+      </Typography>
+      <List disablePadding>
+        {products.map((product) => (
+          <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
+            <ListItemText primary={product.name} secondary={product.desc} />
+            <Typography variant="body2">{product.price}</Typography>
+          </ListItem>
+        ))}
+
+        <ListItem sx={{ py: 1, px: 0 }}>
+          <ListItemText primary="Total" />
+          <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
+            $150.00
+          </Typography>
+        </ListItem>
+      </List>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+            Shipping
+          </Typography>
+          <Typography gutterBottom>John Smith</Typography>
+          <Typography gutterBottom>{addresses.join(', ')}</Typography>
+        </Grid>
+        <Grid item container direction="column" xs={12} sm={6}>
+          <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+            Payment details
+          </Typography>
+          <Grid container>
+            {payments.map((payment) => (
+              <React.Fragment key={payment.name}>
+                <Grid item xs={6}>
+                  <Typography gutterBottom>{payment.name}</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography gutterBottom>{payment.detail}</Typography>
+                </Grid>
+              </React.Fragment>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+}
